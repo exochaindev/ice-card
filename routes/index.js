26876505bc8630d744e97572c034996abda6e15a
Luna <judahiii@gmail.com>
2018-05-01 17:21:46 -0400

    Add /:id/print - presents page with printable card
    Has URL and QR code on one side, all emergency info originally entered
    in small text on the back.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@
 var express = require('express');
 var router = express.Router();
 const model = require('../model/index.js');
+const qr = require('qr-image');
+
+var canonicalDomain = 'localhost:3000';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -25,6 +28,28 @@ router.get('/:uid', function(req, res, next) {
     res.render('debug', { debugString: "Error:\n" + err });
   });
 });
+
+router.get('/:uid/print', function(req, res, next) {
+  let uid = model.sanitizeId(req.params.uid);
+  let url = canonicalDomain + '/' + uid;
+  let qrUrl = '/' + uid + '/qr.svg';
+  model.getCard(uid).then((contacts) => {
+    res.render('print-card', { contacts: contacts, url: url, qrUrl: qrUrl });
+  }, (err) => {
+    res.render('debug', { debugString: "Error:\n" + err });
+  });
+});
+
+router.get('/:uid/qr.:ext', function(req, res, next) {
+  let uid = model.sanitizeId(req.params.uid);
+  let url = canonicalDomain + '/' + uid;
+  let qrSvg = qr.image(url, {
+    type: req.params.ext,
+    size: 6,
+    margin: 0,
+  });
+  qrSvg.pipe(res);
+})
 
 module.exports = router;
 
