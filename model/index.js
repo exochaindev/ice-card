becfda86b58dc7e9cfcaa640e972aa546fd8c9ce
Luna <lucidlyluna@gmail.com>
2018-05-24 17:40:09 -0400

    Add asymmetric keys to cards when password added
    Also add an encryption method to encryptCard, but it's not tested yet.

--- model/index.js
+++ model/index.js
@@ -132,15 +132,35 @@ function canAddSecure(card) {
   return card.secureExpires > Date.now();
 }
 function revokeSecure(id, card) {
+  // Don't allow security to be revoke (=data deleted) by just anyone
   if (!card.secure/* || Some nebulous idea of "authenticated" (TODO) */)
-  card.secureExpires = 0;
-  delete card.secure;
-  delete card.encrypted;
-  updateCard(id, card);
+  {
+    card.secureExpires = 0;
+    delete card.secure;
+    delete card.encrypted;
+    updateCard(id, card);
+  }
 }
-function makeSecure(id, card, password) {
+async function makeSecure(id, card, password) {
   card.secureExpires = 0; // No more making secure / changing password, obviously!
+  // The secure field is entirely encrypted, every time with AES. This keeps
+  // metadata secure
   card.secure = {};
+  // The asymmetric field is for data encrypted with our *public key*
+  // This is initially added so that a friend's key can be split and shared
+  // without needing us there.
+  // This field's fields are not encrypted, but their values are. E.g.:
+  // "asymmetric" : {
+  //   "keys" : {
+  //     "cool-rabbit-12" : "ehtxuntaexn{{RSA encrypted password part}}oent"
+  //   }
+  // }
+  card.asymmetric = {};
+  // Generate an RSA keypair to be used when someone else's key is shared
+  let keypair = await secure.generateEncryptedKeyPair(password);
+  card['publicKey'] = keypair.publicKey;
+  card['privateKeyEncrypted'] = keypair.privateKeyEncrypted;
+  // TODO: Check if we can already share our key (requires concept of identity)
   secure.encryptCard(card, password);
   updateCard(id, card);
 }
--- model/secure.js
+++ model/secure.js
@@ -1,11 +1,39 @@
 'use strict';
 
 const crypto = require('crypto');
+// TODO: Use all forge instead of crypto?
+const forge = require('node-forge');
 
 // AES is secure to known-plaintext, which is absolutely critical for:
 // - Public encrypted databases
 // - JSON
 const cipherType = 'aes192';
+
+function encrypt(plaintext, password) {
+  const cipher = crypto.createCipher(cipherType, Buffer.from(password));
+  let encrypted = cipher.update(plaintext, 'utf8', 'base64');
+  encrypted += cipher.final('base64');
+  return encrypted;
+}
+function decrypt(ciphertext, password) {
+  const cipher = crypto.createDecipher(cipherType, Buffer.from(password));
+  let decrypted = cipher.update(ciphertext, 'base64', 'utf8');
+  decrypted += cipher.final('utf8');
+  return decrypted;
+}
+
+// Find only leaves (bottom-level values) and sets it to callback(leaf)
+function changeLeaves(obj, callback) {
+  for (let key in obj) {
+    let value = key;
+    if (typeof(value) == typeof({})) {
+      changeLeaves(value, callback);
+    }
+    else {
+      obj[key] = callback(value);
+    }
+  }
+}
 
 // card.secure can be in two states:
 // 1. an object, which all those secrets in plain
@@ -18,10 +46,11 @@ function encryptCard(card, password) {
     return card;
   }
   let plaintextJSON = JSON.stringify(card.secure);
-  const cipher = crypto.createCipher(cipherType, Buffer.from(password));
-  let encrypted = cipher.update(plaintextJSON, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
-  card.secure = encrypted;
+  card.secure = encrypt(plaintextJSON, password);
+  let keypair = getKeyPairFromPems(card.publicKey, card.privateKeyEncrypted, password);
+  changeLeaves(card.symmetric, function(val) {
+    return keypair.privateKey.encrypt(val);
+  });
   card.encrypted = true;
   return card; // Modifies in place as always, but return it for convenience
 }
@@ -30,9 +59,7 @@ function decryptCard(card, password) {
     return card;
   }
   let encryptedJSON = card.secure;
-  const cipher = crypto.createDecipher(cipherType, Buffer.from(password));
-  let decrypted = cipher.update(encryptedJSON, 'hex', 'utf8');
-  decrypted += cipher.final('utf8');
+  let decrypted = decrypt(encryptedJSON, password);
   try {
     card.secure = JSON.parse(decrypted);
   }
@@ -43,6 +70,39 @@ function decryptCard(card, password) {
   return card; // Modifies in place as always, but return it for convenience
 }
 
-module.exports.encryptCard = encryptCard;
-module.exports.decryptCard = decryptCard;
+function generateEncryptedKeyPair(password) {
+  // I don't know why forge's method doesn't use Promise, but it seems the
+  // reasonable thing to do
+  return new Promise(function(resolve, reject) {
+    // generate an RSA key pair asynchronously (uses web workers if available)
+    // use workers: -1 to run a fast core estimator to optimize # of workers
+    forge.rsa.generateKeyPair({bits: 2048, workers: -1}, function(err, keypair) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        // wraps and encrypts a Forge private key and outputs it in PEM format
+        keypair.privateKeyEncrypted = forge.pki.encryptRsaPrivateKey(privateKey, password);
+        // Put the public in PEM for JSON storage
+        keypair.publicKey = forge.pki.publicKeyToPem(keypair.publicKey);
+        // TODO: Delete plaintext private key for code safety? Or does convenience win?
+        resolve(keypair);
+      }
+    });
+  });
+}
+function getKeyPairFromPems(publicPem, privateEncryptedPem, password) {
+  let rv = {};
+  // convert a PEM-formatted public key to a Forge public key
+  rv.publicKey = forge.pki.publicKeyFromPem(publicPem);
+  // decrypts a PEM-formatted, encrypted private key
+  rv.privateKey = forge.pki.decryptRsaPrivateKey(privateEncryptedPem, password);
+  return rv;
+}
+
+module.exports = {
+  encryptCard: encryptCard,
+  decryptCard: decryptCard,
+  generateEncryptedKeyPair: generateEncryptedKeyPair,
+};
 
