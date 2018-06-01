f6686a07c54dd92e5e3a21b678496256eac2dbdb
Luna <lucidlyluna@gmail.com>
2018-06-01 15:50:13 -0400

    Change domain to localhost; add escrow to populate

--- util/populate.js
+++ util/populate.js
@@ -1,6 +1,8 @@
 const model = require('../model/index.js');
 const fs = require('fs');
 
+var secureCount = 3;
+
 fs.readFile('./util/test-card.json', 'utf-8', async (err, data) => {
   if (err) {
     throw err;
@@ -9,5 +11,17 @@ fs.readFile('./util/test-card.json', 'utf-8', async (err, data) => {
   id = await model.addCard(card);
   console.log(id);
   console.log('http://' + model.getCardUrl(id, true));
+  for (let entry in card.contacts) {
+    if (entry != 'you') {
+      model.referrerCard(card.contacts.you.key, entry).then((referred) => {
+	model.addCard(referred).then((id) => {
+	  if (secureCount > 0) {
+	    model.makeSecure(referred, 'password');
+	    secureCount--;
+	  }
+	});
+      });
+    }
+  }
 });
 
